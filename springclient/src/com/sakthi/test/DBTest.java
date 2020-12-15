9d24964f22bf2b37ec42f63df368b5bfe65f6041
sakthivelcse2506 <sakthivelcse2506@gmail.com>
2020-12-15 17:25:22 +0530

    chenaged out

--- springclient/src/com/sakthi/test/DBTest.java
+++ springclient/src/com/sakthi/test/DBTest.java
@@ -9,7 +9,7 @@ public class DBTest {
 			System.err.println("Establishing the DB connection...");
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","TENJIN", "TENJIN123");
-			System.err.println("Conneted successfully...");
+			System.out.println("Conneted successfully...");
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		} 
